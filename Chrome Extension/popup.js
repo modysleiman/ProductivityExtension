62444ac078a2051ba7dc90ee2c8d053f26a5096a
cmckenzie16 <camckenzie14@gmail.com>
2018-03-31 21:54:39 -0700

    Add files via upload

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,25 +1,37 @@
+	
 	$(document).ready(function() {   // load the function after DOM ready.
 
 
 	var link1=chrome.extension.getURL("img/logo.png");     //get absolute path of the file residing your extension.
 	var t1='<div id="pop"><a href=""><input type="button" id="quotes" style="width: 110%" value="Dont Let Social Media Get You Down"/></a></div>'    //now set the src to absolute path.
-	$(".StaticLoggedOutHomePage").prepend(t1);     //insert icon into corner
+	$(".StaticLoggedOutHomePage").prepend(t1);    //insert icon into corner*/
 
+	var closeButton = chrome.extension.getURL("img/close.png");
 
 	var link3=chrome.extension.getURL("img/logo.png");
 	var t2='<div id="pop_bg" style="height: 100%; width: 100%;"></div><div id="mgt_popup"><span class="popup_close"></span><div><img id="logo" src="'+link3+'"/></div><div id="pop_inner"><label id="user"></label><input type="button" id="mindful" value="Be Mindful"/></div></div>'
 	$(".StaticLoggedOutHomePage").append(t2);
+
 
 	$("#pop").click(function() {
 	event.preventDefault(); //first stop default behaviour of anchor.
 	$("#pop_bg").css("display", "block"); //show pop-up background.
 	$("#mgt_popup").css("display", "block"); //show pop-up div.
 	$("#user").html("Don't let social media affect your mental health. Stay present. Stay mindful.<br><br>");
+
+	var randomStats = [
+	"Apples are good",
+	"Bananas are bad"
+	];
+
+	var randomNumber = Math. floor(Math.random()*randomStats.length);
+	var randStat = randomStats[randomNumber];
+
+	$("#user").html(randStat);
 	});
 
-	var close=chrome.extension.getURL("img/close.png")
-	
-	$(".popup_close").click(function(){ //click close symbol hides popup
+
+	$("#pop_bg").click(function(){  //click close symbol hides popup
 	$("#pop_bg").css("display", "none");
 	$("#mgt_popup").css("display", "none");
 	});
